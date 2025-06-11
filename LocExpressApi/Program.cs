768a227d194aaa3dace4679fd21d1a9cbc3ecc6f
CDA2 <youri.muller@ccicampus.fr>
2025-06-11 11:18:34 +0200

    Api and database working together in docker

--- LocExpressApi/Program.cs
+++ LocExpressApi/Program.cs
@@ -4,19 +4,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<MyDbContext>(options =>
-    options.UseNpgsql("Host=localhost;Port=5460;Username=locexpress;Password=postgres;Database=locexpress"));
+    options.UseNpgsql("Host=loc_express_db;Port=5432;Username=locexpress;Password=postgres;Database=locexpress"));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
